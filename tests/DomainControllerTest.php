ab70ee82e3e6c488ac7bf5ad12c5952aecc89bc4
moklidia <kitsunegakkou@gmail.com>
2019-08-15 19:16:58 +0400

    add guzzle test

--- tests/DomainControllerTest.php
+++ tests/DomainControllerTest.php
@@ -2,6 +2,14 @@
 
 namespace Tests;
 
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use GuzzleHttp\Psr7\Response;
+use App\Http\Controllers\DomainController;
+use App\Domain;
+
 class DomainControllerTest extends TestCase
 {
     
@@ -15,5 +23,25 @@ class DomainControllerTest extends TestCase
     {
         $this->get('domains');
         $this->assertResponseStatus(200);
+    }
+
+        public function testDomainsClient()
+    {
+        $body = file_get_contents(__DIR__ . '/fixtures/test.html');
+        $mock = new MockHandler([
+            new Response(200, ['Content-Length' => 15], $body)
+        ]);
+        $handler = HandlerStack::create($mock);
+        $controller = new DomainController();
+        $request = new Request();
+        $request->merge(['name' => __DIR__ . '/fixtures/test.html']);
+        $controller->store($request);
+        $this->seeInDatabase('domains', [
+            'name' => __DIR__ . '/fixtures/test.html',
+            'contentLength' => "15",
+            'body' => $body,
+            'h1' => 'Heading',
+            'description' => 'A simple html page to test app API'
+        ]);
     }
 }
