72596ebca013a5b9dcfa1c5bf027d180d5cf0982
moklidia <kitsunegakkou@gmail.com>
2019-08-15 19:31:28 +0400

    test refactor

--- tests/DomainControllerTest.php
+++ tests/DomainControllerTest.php
@@ -7,13 +7,6 @@ use App\Domain;
 
 class DomainControllerTest extends TestCase
 {
-    
-    public function testRedirectAfterDomainPost()
-    {
-        $this->post('/domains', ['name' => 'https://www.google.com']);
-        $this->assertResponseStatus(302);
-    }
-
     public function testDomainIndex()
     {
         $this->get('domains');
@@ -25,5 +18,6 @@ class DomainControllerTest extends TestCase
         $url = 'https://www.facebook.com';
         $this->post('/domains', ['name' => $url]);
         $this->SeeInDatabase('domains', ['name' => $url]);
+        $this->assertResponseStatus(302);
     }
 }
