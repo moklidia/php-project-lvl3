b10f36b204a0702205f1775a87cdc3be37b4f597
moklidia <kitsunegakkou@gmail.com>
2019-08-16 14:26:44 +0400

    fix lint errors

--- app/Jobs/SendRequestToDomainJob.php
+++ app/Jobs/SendRequestToDomainJob.php
@@ -32,10 +32,10 @@ class SendRequestToDomainJob extends Job
             $this->domain->statusCode = $response->getStatusCode();
             $this->domain->reject();
             $this->domain->save();
-        }
-        else {
+        } else {
             $this->domain->statusCode = $response->getStatusCode();
-            $this->domain->contentLength = isset($response->getHeader('Content-Length')[0]) ? $response->getHeader('Content-Length')[0] : 'unknown';
+            $this->domain->contentLength = isset($response->getHeader('Content-Length')[0]) ?
+                                           $response->getHeader('Content-Length')[0] : 'unknown';
             $this->domain->body = $response->getBody()->getContents();
             dispatch(new ParseHtmlJob($this->domain));
         }
