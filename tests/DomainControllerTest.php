26c6a5445a25c55cd711e8114f0cd9635a816aef
moklidia <kitsunegakkou@gmail.com>
2019-08-21 18:45:04 +0400

    add domain.show test

--- tests/DomainControllerTest.php
+++ tests/DomainControllerTest.php
@@ -2,21 +2,33 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 use App\Domain;
 
 class DomainControllerTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function testDomainIndex()
     {
         $this->get('domains');
         $this->assertResponseStatus(200);
     }
 
+    public function testDomainShow()
+    {
+        $domain = Domain::create(['name' => 'testdomain.com', 'state' => 'approved']);
+        $domain->save();
+        $this->get('/domains/' . $domain->id);
+        $this->assertResponseStatus(200);
+
+    }
+
     public function testDomainStore()
     {
         $url = 'https://www.facebook.com';
-        $this->post('/domains', ['name' => $url]);
+        $this->post('domains', ['name' => $url]);
         $this->SeeInDatabase('domains', ['name' => $url]);
         $this->assertResponseStatus(302);
     }
