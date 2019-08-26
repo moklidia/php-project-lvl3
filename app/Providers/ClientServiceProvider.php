a463da4f5c550d509c7519b40adb24a26d6958b3
moklidia <kitsunegakkou@gmail.com>
2019-08-26 10:06:57 +0400

    refactor tests

--- app/Providers/ClientServiceProvider.php
+++ app/Providers/ClientServiceProvider.php
@@ -4,9 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Response;
 
 class ClientServiceProvider extends ServiceProvider
 {
@@ -17,19 +14,8 @@ class ClientServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (app()->environment('testing')) {
-            $body = file_get_contents('/Users/admin/hexlet/php/page-analyzer/tests/fixtures/test.html');
-            $mock = new MockHandler([
-                new Response(200, ['Content-Length' => 15], $body)
-            ]);
-            $handler = HandlerStack::create($mock);
-            $this->app->bind('GuzzleHttp\Client', function ($app) use ($handler) {
-                return new Client(['handler' => $handler]);
-            });
-        } else {
-            $this->app->bind('GuzzleHttp\Client', function ($app) {
-                return new Client(['http_errors' => false]);
-            });
-        }
+        $this->app->bind('GuzzleHttp\Client', function ($app) {
+            return new Client(['http_errors' => false]);
+        });
     }
 }
--- tests/DomainControllerTest.php
+++ tests/DomainControllerTest.php
@@ -4,13 +4,30 @@ namespace Tests;
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
-use App\Http\Controllers\DomainController;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use App\Domain;
 
 class DomainControllerTest extends TestCase
 {
     use DatabaseMigrations;
+
+    protected function setUp():void
+    {
+        parent::setUp();
+        $path = __DIR__ . '/fixtures/test.html';
+        $body = file_get_contents($path);
+        $mock = new MockHandler([
+            new Response(200, ['Content-Length' => 15], $body)
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->app->bind('GuzzleHttp\Client', function ($app) use ($handler) {
+            return new Client(['handler' => $handler]);
+        });
+    }
 
     public function testDomainIndex()
     {
@@ -20,7 +37,8 @@ class DomainControllerTest extends TestCase
 
     public function testDomainShow()
     {
-        $domain = Domain::create(['name' => 'testdomain.com', 'state' => 'approved']);
+        $domain = factory('App\Domain')->make();
+        $domain->approve();
         $domain->save();
         $this->get('/domains/' . $domain->id);
         $this->assertResponseStatus(200);
